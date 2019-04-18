c8031ae4447212e9c206e3bed4cb4d026f4422e1
wangwei <wangwei@myzaker.com>
2019-04-18 15:51:39 +0800

    使用优先队列的多向归并

---
+++ src/chapter2/Multiway.java
@@ -0,0 +1,37 @@
+package chapter2;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ *  使用优先队列的多向归并
+ */
+public class Multiway {
+    public static void merge(In[] streams) {
+        int N = streams.length;
+        IndexMinPQ<String> pq = new IndexMinPQ<>(N);
+
+        for (int i = 0; i < N; i++) {
+            pq.insert(i, streams[i].readString());
+        }
+
+        while (!pq.isEmpty()) {
+            StdOut.println(pq.minKey());
+            int i = pq.delMin();
+
+            if (!streams[i].isEmpty()) {
+                pq.insert(i, streams[i].readString());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int N = args.length;
+        In[] streams = new In[N];
+        for (int i = 0; i < N; i++) {
+            streams[i] = new In(args[i]);
+        }
+
+        merge(streams);
+    }
+}
