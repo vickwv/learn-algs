f1720fa08888fa4625a7109dfb76089fdd143c80
stevenvww <stevenv.w.w@gmail.com>
2020-01-08 08:51:30 +0800

    对角线遍历(迭代反转法)

--- src/leetcode/array/FindDiagonalOrder.java
+++ src/leetcode/array/FindDiagonalOrder.java
@@ -9,7 +9,14 @@ import java.util.Collections;
  * @url https://leetcode-cn.com/problems/diagonal-traverse/
  */
 public class FindDiagonalOrder {
-    //  对角线遍历暴力简单版本
+    /**
+     * 对角线迭代反转法
+     * 1. 遍历每个对角线
+     * 2. 在将元素添加到最终结果数组之前，我们只需要将奇数对角线反转即可。
+     * 例如，从左边开始的第三个对角线是[3,7,11] ，在将这些元素添加到最终的结果数组之前，我们只需将它们颠倒过来，即[11,7,3]。
+     * @param matrix int[][]
+     * @return
+     */
     public int[] findDiagonalOrder(int[][] matrix) {
         if (matrix == null || matrix.length == 0) {
             return new int[0];
@@ -23,15 +30,15 @@ public class FindDiagonalOrder {
         int index = 0;
         for (int d = 0; d < N+M+1; d++) {
             temp.clear();
-            int x = d < M ? 0 : d - M + 1;
-            int y = d < M ? d : M - 1;
-            while (x < N && y > -1) {
+            int x = d < M ? 0 : d - M + 1; // 计算x轴的规律
+            int y = d < M ? d : M - 1; // 计算y轴的规律
+            while (x < N && y > -1) { //将单个对角线放进临时链表
                 temp.add(matrix[x][y]);
                 x++;
                 y--;
             }
 
-            // d 为奇数需要反转
+            // d 为奇数需要反转，[3,7,11] => [11,7,3]
             if (d % 2 == 0) {
                 Collections.reverse(temp);
             }
