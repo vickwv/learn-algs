b8c42d5dd56987c8b323a065ac6804b98ebb9897
stevenvww <stevenv.w.w@gmail.com>
2020-03-02 21:02:53 +0800

    反转单链表

--- src/leetcode/recurision/ReverseSingleLinkedList.java
+++ src/leetcode/recurision/ReverseSingleLinkedList.java
@@ -2,6 +2,8 @@ package leetcode.recurision;
 
 /**
  * 反转单链表
+ * solution1: 纯粹递归反转
+ * solution2: 迭代反转
  *
  */
 public class ReverseSingleLinkedList {
@@ -13,12 +15,34 @@ public class ReverseSingleLinkedList {
         }
     }
 
-    public ListNode reverseList(ListNode head)
+    /**
+     * 5->null
+     * 5->4->null
+     * 5->4->3->null
+     * @param head
+     * @return
+     */
+    public ListNode Solution1(ListNode head)
     {
+        // 假设有个单链表 1->2->3->4->5,当前curr=5,则head=4.
         if (head == null || head.next == null) return head;
-        ListNode p = reverseList(head.next);
+        ListNode curr = Solution1(head.next);
+        // 这里把5->4->null
         head.next.next = head; // 当前结点的下一个结点指向当前结点
         head.next = null; // 当前结点的下一个结点指向Null
-        return p;
+        return curr;
+    }
+
+    public ListNode Solution2(ListNode head)
+    {
+        ListNode prev = null; // 用来存储已经反转的节点
+        ListNode current = head; // 用来存储下一个节点
+        while (current != null ) {
+            ListNode tmp = current.next; // 因为current.next->prev，所以要先存储Next节点
+            current.next = prev;
+            prev = current;
+            current = tmp;
+        }
+        return prev;
     }
 }
