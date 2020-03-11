bbf3c1b7b419f9791378a90150a9fa1812d5cbc6
stevenvww <stevenv.w.w@gmail.com>
2020-03-11 18:36:58 +0800

    旋转数组

---
+++ src/leetcode/array/rotate.java
@@ -0,0 +1,45 @@
+package leetcode.array;
+
+/**
+ * 旋转数组
+ * Solution1:暴力循环法
+ * Solution2: 反转所有数，再反转前k个数，再反转后i-k个数
+ * Solution3: 原本数组里下标为 i 的我们把它放到 (i+k)\%数组长度(i+k)%数组长度 的位置。然后把新的数组拷贝到原数组中。
+ */
+public class rotate {
+    public void solution1(int[] nums, int k) {
+        for (int i = 0; i < k; i++) {
+            int pre = nums[nums.length-1];
+            for (int j = 0; j < nums.length; j++) {
+                int temp = nums[i];
+                nums[i] = pre;
+                pre = temp;
+            }
+        }
+    }
+
+    public void solution2(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+    }
+
+    private void reverse(int[] nums, int k, int i) {
+        while (k < i) {
+            int temp = nums[k];
+            nums[k++] = nums[i];
+            nums[i--] = temp;
+        }
+    }
+
+    public void solution3(int[] nums, int k) {
+        int[] temp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            temp[(i+k)%nums.length] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
