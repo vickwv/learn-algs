794d184384dfe5471b3037ae8334da16d087ac9e
stevenv <stevenv.w.w@gmail.com>
2022-03-20 19:45:30 +0800

    Thread: ParkAndUnParkDemo

---
+++ src/java_foundation/thread/ParkAndUnParkDemo.java
@@ -0,0 +1,45 @@
+package java_foundation.thread;
+
+import java.util.concurrent.locks.LockSupport;
+
+class ParkThread extends Thread {
+    private Object object;
+
+    public ParkThread(Object object) {
+        this.object = object;
+    }
+
+    public void run() {
+        System.out.println("before unpark");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // 获取blocker
+        System.out.println("Blocker info " + LockSupport.getBlocker((Thread) object));
+        // 释放许可
+        LockSupport.unpark((Thread) object);
+        // 休眠500ms，保证先执行park中的setBlocker(t, null);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // 再次获取blocker
+        System.out.println("Blocker info " + LockSupport.getBlocker((Thread) object));
+
+        System.out.println("after unpark");
+    }
+}
+
+public class ParkAndUnParkDemo {
+    public static void main(String[] args) throws InterruptedException {
+        ParkThread myThread = new ParkThread(Thread.currentThread());
+        myThread.start();
+        System.out.println("before park");
+        // 获取许可
+        LockSupport.park("ParkAndUnparkDemo");
+        System.out.println("after park");
+    }
+}
